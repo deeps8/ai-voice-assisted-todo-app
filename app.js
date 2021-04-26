1f43d4cf5aff88fe452292ed89981c0f98a9fecc
deeps8 <sharmadeepak731998@gmail.com>
2021-04-26 17:11:50 +0530

    app issue

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ app.use((err,req,res,next)=>{
     });
 });
 
-app.listen(process.env.PORT || 8080, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
